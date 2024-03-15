780a026446b31d6c21d967551aced8304b78cdcf
Ankit Kumar Sahu <sahuankit.murhu@gmail.com>
2024-03-16 02:05:46 +0530

    adding literal, numeric and identifier scanners

--- com/piscan/Zealot/Scanner.java
+++ com/piscan/Zealot/Scanner.java
@@ -8,6 +8,32 @@ import java.util.Map;
 import static com.piscan.Zealot.TokenType.*;
 
 class Scanner {
+
+    // creating a map of identifier tokens
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+
+        keywords.put("and", AND);
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
+    
     private final String source;
 
     // creating a list of tokens
@@ -17,23 +43,238 @@ class Scanner {
     private int current = 0;
     private int line = 1;
 
-    // scanning the tokens
+    // scanning the tokens as string
     Scanner(String source) {
         this.source = source;
     }
 
-    List<Token> scanTokens(){
-        while (!isAtEnd) {
+    List<Token> scanTokens() {
+
+        // we are at the beginning of the lexeme
+        while (!isAtEnd()) {
             start = current;
+
+            // we will scan tokens here
             scanTokens();
         }
 
+        // we are basically adding the scanned tokens to the list
         tokens.add(new Token(EOF, "", null, line));
         return tokens;
     }
 
     private boolean isAtEnd() {
         return current >= source.length();
+    }
+
+    // Identifing the scanned tokens
+    private void scanToken() {
+
+        // we are making current pointer point to the next character
+        char c = advance();
+        switch (c) {
+            case '(':
+                addToken(LEFT_PAREN);
+                break;
+            case ')':
+                addToken(RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(LEFT_BRACE);
+                break;
+            case '}':
+                addToken(RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(COMMA);
+                break;
+            case '.':
+                addToken(DOT);
+                break;
+            case '-':
+                addToken(MINUS);
+                break;
+            case '+':
+                addToken(PLUS);
+                break;
+            case ';':
+                addToken(SEMICOLON);
+                break;
+            case '*':
+                addToken(STAR);
+                break;
+
+            // to handle the case of != , <= ,>= and all we are checking the next character
+            // after every check
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+
+            /*
+             * This is similar to the other two-character operators, except that when we
+             * find a second /, we don’t end the token yet. Instead, we keep consuming
+             * characters until we reach the end of the line.
+             */
+            case '/':
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd())
+                        advance();
+                } else {
+                    addToken(SLASH);
+                }
+                break;
+
+            // Ignoring whitespaces
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+            case '\n':
+                line++;
+                break;
+
+            // handling strings
+            case '"':
+                string();
+                break;
+
+            // it handles the identifiers like or
+            case 'o':
+                if (match('r')) {
+                    addToken(OR);
+                }
+                break;
+
+            // for handling the lexemes that are not available on the language like @ ^ #
+            default:
+
+                if (isDigit(c)) {
+                    number();
+                }
+
+                else if (isAlpha(c)) {
+                    identifier();
+                }
+
+                else {
+                    Zealot.error(line, "Unexpected character.");
+                }
+                break;
+        }
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek()))
+            advance();
+        String text = source.substring(start, current);
+        
+        // Extracts the token type of the character
+        TokenType type = keywords.get(text);
+        if (type == null) {
+            type = IDENTIFIER;
+        }
+
+        addToken(IDENTIFIER);
+    }
+
+    // checking for numbers
+    private void number() {
+
+        while (isDigit(peek()))
+            advance();
+
+        // looking for the fractional part
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+
+            while (isDigit(peek()))
+                advance();
+        }
+
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+
+    }
+
+    // Checking for strings
+    private void string() {
+
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n')
+                line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Zealot.error(line, "Unterminated String.");
+            return;
+        }
+
+        // the closing "
+        advance();
+
+        /* we are only taking the value between the double quotes */
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
+
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd())
+            return false;
+        if (source.charAt(current) != expected)
+            return false;
+
+        current++;
+        return true;
+    }
+
+    private char peek() {
+        if (isAtEnd())
+            return '\0';
+        return source.charAt(current);
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length())
+            return '\0';
+        return source.charAt(current + 1);
+    }
+
+    // this checks if the scanned lexeme is a alphabet or not
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    // This function makes the current pointer point to the next Lexeme on every
+    // iteration
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
     }
 
 }
--- com/piscan/Zealot/Zealot.java
+++ com/piscan/Zealot/Zealot.java
@@ -7,7 +7,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Zealot {
 
@@ -15,9 +15,13 @@ public class Zealot {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
             System.exit(64);
-        } else if (args.length == 1) {
+        }
+
+        else if (args.length == 1) {
             runFile(args[0]);
-        } else {
+        }
+
+        else {
             runPrompt();
         }
     }
@@ -26,66 +30,60 @@ public class Zealot {
     // We’ll use this to ensure we don’t try to execute code that has a known error.
     static boolean hadError = false;
 
-}
+    // Running with prompt
 
-// Running with prompt
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
 
-private static void runFile(String path) throws IOException{
-    byte[] bytes = Files.readAllBytes(Paths.get(path));
-    run(new String(bytes , Charset.defaultCharset()));
-
-    // Indicate an error in the exit code.
-    if (hadError) 
-        System.exit(65);
-}
-
-
-// Running without any prompt
-
-private static void runPrompt() throws IOException {
-
-    // Bridging byte stream and Character Stream
-    InputStreamReader input = new InputStreamReader(System.in);
-    
-    // Extracting text from character stream
-    BufferedReader reader = new BufferedReader(input);
-
-    for(;;){
-        System.out.print("> ");
-        
-        //Reading one line at a time
-        String line = reader.readLine();
-        if(line == null)
-            break;
-        run(line);
-        hadError = false;
+        // Indicate an error in the exit code.
+        if (hadError)
+            System.exit(65);
     }
-}
 
-private static void run(String source) {
-    Scanner scanner = new Scanner(source);
-    
-    // Creating token
-    List<Token> tokens = scanner.scanTokens();
+    // Running without any prompt
 
-    // Printing all tokens
-    for(Token token : tokens){
-        System.out.println(token);
+    private static void runPrompt() throws IOException {
+
+        // Bridging byte stream and Character Stream
+        InputStreamReader input = new InputStreamReader(System.in);
+
+        // Extracting text from character stream
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+
+            // Reading one line at a time
+            String line = reader.readLine();
+            if (line == null)
+                break;
+            run(line);
+            hadError = false;
+        }
     }
-    
-}
 
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
 
+        // Creating token
+        List<Token> tokens = scanner.scanTokens();
 
+        // Printing all tokens
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
 
+    }
 
-// Finding Error
-static void error(int line, String message){
-    report(line , "", message);
-}
+    // Finding Error
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
 
-// Reporting Error
-private static void report(int line , String where, String message) {
-    System.err.println("[line " + "] Error" + where + ": " + message);
-    hadError = true;
+    // Reporting Error
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + "] Error" + where + ": " + message);
+        hadError = true;
+    }
 }
